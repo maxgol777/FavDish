f771794049c6de4181dbc0a7581086df23ce1550
Maksym Golovanov <maxgol777@users.noreply.github.com>
2021-12-11 22:11:46 +0200

    View Binding

--- app/src/main/java/com/maxgol/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/maxgol/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,24 @@
 package com.maxgol.favdish.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.maxgol.favdish.R
+import androidx.appcompat.app.AppCompatActivity
+import com.maxgol.favdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
