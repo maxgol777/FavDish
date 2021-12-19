6d8af901a87f3aaeef6a278aa979dced502c07e6
Maxgol <maxgol777@gmail.com>
2021-12-19 10:32:11 +0200

    Get all dishes

--- app/src/main/java/com/maxgol/favdish/model/database/FavDishDao.kt
+++ app/src/main/java/com/maxgol/favdish/model/database/FavDishDao.kt
@@ -2,10 +2,15 @@ package com.maxgol.favdish.model.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.maxgol.favdish.model.entities.FavDish
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavDishDao {
     @Insert
     suspend fun insertFavDishDetails(favDish: FavDish)
+
+    @Query("SELECT * FROM FAV_DISHES_TABLE ORDER BY ID")
+    fun getAllDishesList(): Flow<List<FavDish>>
 }
