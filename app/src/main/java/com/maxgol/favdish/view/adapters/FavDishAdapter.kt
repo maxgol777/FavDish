25828e4363f749655281d9b2e74cbf9f3b977ad8
Maxgol <maxgol777@gmail.com>
2021-12-20 10:56:22 +0200

    FavDish fragment

---
+++ app/src/main/java/com/maxgol/favdish/view/adapters/FavDishAdapter.kt
@@ -0,0 +1,42 @@
+package com.maxgol.favdish.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.maxgol.favdish.databinding.ItemDishLayoutBinding
+import com.maxgol.favdish.model.entities.FavDish
+
+class FavDishAdapter(private val fragment: Fragment) :
+    RecyclerView.Adapter<FavDishAdapter.ViewHolder>() {
+
+    private var dishes: List<FavDish> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemDishLayoutBinding = ItemDishLayoutBinding.inflate(
+            LayoutInflater.from(fragment.context), parent, false
+        )
+        return ViewHolder((binding))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dish = dishes[position]
+        holder.tvTitle.text = dish.title
+        Glide.with(fragment)
+            .load(dish.image)
+            .into(holder.ivDishImage)
+    }
+
+    override fun getItemCount(): Int = dishes.size
+
+    fun dishesList(list: List<FavDish>) {
+        dishes = list
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root) {
+        val ivDishImage = view.ivDishImage
+        val tvTitle = view.tvDishTitle
+    }
+}
--- app/src/main/java/com/maxgol/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/maxgol/favdish/view/fragments/AllDishesFragment.kt
@@ -2,23 +2,22 @@ package com.maxgol.favdish.view.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.maxgol.favdish.R
 import com.maxgol.favdish.application.FavDishApplication
+import com.maxgol.favdish.databinding.FragmentAllDishesBinding
 import com.maxgol.favdish.view.activities.AddUpdateDishActivity
+import com.maxgol.favdish.view.adapters.FavDishAdapter
 import com.maxgol.favdish.viewmodel.FavDishViewModel
 import com.maxgol.favdish.viewmodel.FavDishViewModelFactory
-import com.maxgol.favdish.viewmodel.HomeViewModel
 
 class AllDishesFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var mBinding: FragmentAllDishesBinding
 
     private val mFavDishViewModel by viewModels<FavDishViewModel> {
         FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
@@ -33,25 +32,34 @@ class AllDishesFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_all_dishes, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    ): View {
+//        homeViewModel =
+//            ViewModelProvider(this).get(HomeViewModel::class.java)
+//        val root = inflater.inflate(R.layout.fragment_all_dishes, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        mBinding = FragmentAllDishesBinding.inflate(inflater, container, false)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.i("Dish Title", "OnCreated")
+
+        mBinding.rvDishesList.layoutManager = GridLayoutManager(requireActivity(), 2)
+        val favDishAdapter = FavDishAdapter(this)
+        mBinding.rvDishesList.adapter = favDishAdapter
+
+
         mFavDishViewModel.allDishesList.observe(viewLifecycleOwner, Observer { dishes ->
-            dishes?.let {
-                for (item in dishes) {
-                    Log.i("Dish Title", "${item.id} :: ${item.title}")
-                }
+            if (dishes.isNotEmpty()) {
+                mBinding.rvDishesList.visibility = View.VISIBLE
+                mBinding.tvNoDishesAddedYet.visibility = View.GONE
+                favDishAdapter.dishesList(dishes)
+            } else {
+                mBinding.rvDishesList.visibility = View.GONE
+                mBinding.tvNoDishesAddedYet.visibility = View.VISIBLE
             }
         })
     }
