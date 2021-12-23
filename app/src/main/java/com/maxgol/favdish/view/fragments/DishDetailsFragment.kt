c705c7c46c7e4d3106278b00a720bea1b221c3e4
Maxgol <maxgol777@gmail.com>
2021-12-23 11:31:29 +0200

    Improve DishDetailsFragment.kt

--- app/src/main/java/com/maxgol/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/maxgol/favdish/view/fragments/DishDetailsFragment.kt
@@ -7,16 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.maxgol.favdish.R
+import com.maxgol.favdish.databinding.FragmentDishDetailsBinding
+import java.io.IOException
+import java.util.*
 
 class DishDetailsFragment : Fragment() {
+
+    private var mBinding: FragmentDishDetailsBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+    ): View {
+        mBinding = FragmentDishDetailsBinding.inflate(inflater, container, false)
+        return mBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,5 +30,29 @@ class DishDetailsFragment : Fragment() {
         val args: DishDetailsFragmentArgs by navArgs()
         Log.i("Dish Title", args.dishDetails.title)
         Log.i("Dish Type", args.dishDetails.type)
+        try {
+            Glide.with(requireActivity())
+                .load(args.dishDetails.image)
+                .centerCrop()
+                .into(mBinding!!.ivDishImage)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        with(mBinding!!) {
+            tvTitle.text = args.dishDetails.title
+            tvType.text = args.dishDetails.type.capitalize(Locale.ROOT)
+            tvCategory.text = args.dishDetails.category
+            tvIngredients.text = args.dishDetails.ingredients
+            tvCookingDirection.text = args.dishDetails.directionToCook
+            tvCookingTime.text = resources.getString(
+                R.string.lbl_estimate_cooking_time,
+                args.dishDetails.cookingTime
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
